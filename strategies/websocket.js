085d63ecbb095df52a0528f91888a50ece6a9f36
t3rr0r <mail@t3rr0r.com>
2014-10-24 11:00:13 -0400

    added websocket strategy

---
+++ strategies/websocket.js
@@ -0,0 +1,21 @@
+module.exports = function(options) {
+    var url = options.url;
+    var WebSocket = require('ws');
+    
+    return function(cb) {
+	var ws = new WebSocket(url);
+	var returned = false;
+
+	ws.on('open', function() {
+	    if (!returned) cb();
+	    returned = true;
+	    ws.close();
+	});
+
+	ws.on('error', function() {
+	    if (!returned) cb('error');
+	    returned = true;
+	    ws.close();
+	});
+    };
+};
