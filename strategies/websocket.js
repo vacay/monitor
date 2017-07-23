09ec68321f01f235a010d9c5fc9ef3282bf01fa7
t3rr0r <mail@t3rr0r.com>
2017-07-23 13:16:19 -0400

    pass in strictSSL option to websocket strategy

--- strategies/websocket.js
+++ strategies/websocket.js
@@ -1,21 +1,23 @@
 module.exports = function(options) {
-    var url = options.url;
-    var WebSocket = require('ws');
-    
-    return function(cb) {
-	var ws = new WebSocket(url);
-	var returned = false;
+  var url = options.url;
+  var WebSocket = require('ws');
 
-	ws.on('open', function() {
-	    if (!returned) cb();
-	    returned = true;
-	    ws.close();
-	});
+  return function(cb) {
+    var ws = new WebSocket(url, {
+      strictSSL: optsions.strictSSL
+    });
+    var returned = false;
 
-	ws.on('error', function() {
-	    if (!returned) cb('error');
-	    returned = true;
-	    ws.close();
-	});
-    };
+    ws.on('open', function() {
+      if (!returned) cb();
+      returned = true;
+      ws.close();
+    });
+
+    ws.on('error', function() {
+      if (!returned) cb('error');
+      returned = true;
+      ws.close();
+    });
+  };
 };
